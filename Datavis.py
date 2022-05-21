6aec5f5be134520c9bb4e1139bf9ea1df9f03042
zainab hodroj <92784831+zainabhodroj@users.noreply.github.com>
2022-05-21 11:42:31 +0300

    Update Datavis.py

--- Datavis.py
+++ Datavis.py
@@ -4,7 +4,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 showWarningOnDirectExecution = False
 st.set_option('deprecation.showPyplotGlobalUse', False)
-
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #faebd7;">
+  <a class="Dataset" href="https://www.kaggle.com/datasets/blastchar/telco-customer-churn" target="_blank">Dataset</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="#">Home <span class="sr-only"></span></a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="https://share.streamlit.io/zainabhodroj/churn/main/home.py" target="_blank">Overview</a>
+      </li>
+      <li class="nav-item"> 
+        <a class="nav-link" href="https://share.streamlit.io/zainabhodroj/datavis/main/Datavis.py" target="_blank">DataVisulization</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://share.streamlit.io/zainabhodroj/churn/main/Modelapp.py" target="_blank">Model Prediction</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
 st.header("Telco Customer Churn Prediction :bar_chart:")
 
 df = pd.read_csv("Churn.csv")
