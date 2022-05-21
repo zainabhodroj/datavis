e78f13d085514e389c460c1c6b2625295304d26b
zainab hodroj <92784831+zainabhodroj@users.noreply.github.com>
2022-05-21 10:45:48 +0300

    Update Datavis.py

--- Datavis.py
+++ Datavis.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+showWarningOnDirectExecution = False
 
 st.header("Telco Customer Churn Prediction :bar_chart:")
 
